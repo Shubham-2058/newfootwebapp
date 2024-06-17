b46e43045eab277fbc155b3415fdd8f0478efb85
SHUBHAM-2058 <bhattaraishubham@gmail.com>
2024-06-17 12:54:50 +0545

    index

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     window.location.href = 'second-page.html';
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const enter = document.getElementById('enter-button');
+
+
+  enter.addEventListener('click', function() {
+    window.location.href = 'second-page.html';
+});
+});
